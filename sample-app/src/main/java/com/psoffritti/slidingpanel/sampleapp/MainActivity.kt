cda50cd2cbdbd0bf6d25ffa036f15adac3c5e96e
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2019-02-07 09:57:29 +0100

    closes #12

--- sample-app/src/main/java/com/psoffritti/slidingpanel/sampleapp/MainActivity.kt
+++ sample-app/src/main/java/com/psoffritti/slidingpanel/sampleapp/MainActivity.kt
@@ -22,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         intent.putExtra(Constants.TITLE.name, getString(R.string.app_name))
         intent.putExtra(Constants.GITHUB_URL.name, "https://github.com/PierfrancescoSoffritti/sliding-panel")
+        intent.putExtra(Constants.HOMEPAGE_URL.name, "https://github.com/PierfrancescoSoffritti/sliding-panel")
+        intent.putExtra(Constants.PLAYSTORE_PACKAGE_NAME.name, "com.psoffritti.slidingpanel.sampleapp")
 
         val examples = arrayOf(
             ExampleActivityDetails(
